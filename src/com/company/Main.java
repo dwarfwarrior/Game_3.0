b57741c74ea8800cb8b87b1b6a4145014c5e4ff3
dwarfwarrior <dwarfwarrior@ukr.net>
2016-10-10 02:12:28 +0300

    third version of my game

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.company.createOrContinue.Chooser;
+import com.company.dataBaseOfGame.ConnectAndWordWhithDB;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        Chooser.Chooser();
+        ConnectAndWordWhithDB.DataBase();
+    }
+}
---
+++ src/com/company/start/level_1/Level_1.java
@@ -0,0 +1,7 @@
+package com.company.start.level_1;
+
+/**
+ * Created by dwarfawrrior on 6/10/2016.
+ */
+public class Level_1 {
+}
