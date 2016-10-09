6fcedb9dd878bb49e50da04f3248ed2ce3c27790
dwarfwarrior <dwarfwarrior@ukr.net>
2016-10-10 02:28:52 +0300

    third version of my game

--- src/com/company/start/headNote/HeadNote.java
+++
@@ -1,7 +0,0 @@
-package com.company.start.headNote;
-
-/**
- * Created by dwarfawrrior on 6/10/2016.
- */
-public class HeadNote {
-}
--- src/com/company/start/level_1/Level_1.java
+++
@@ -1,7 +0,0 @@
-package com.company.start.level_1;
-
-/**
- * Created by dwarfawrrior on 6/10/2016.
- */
-public class Level_1 {
-}
