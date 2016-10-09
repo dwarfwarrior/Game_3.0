1e4c8c3a7272551a1ccfe08a17b7fc2e59a363ca
dwarfwarrior <dwarfwarrior@ukr.net>
2016-10-10 02:32:00 +0300

    third version of my game

---
+++ src/main/java/com/company/start/CreateNewCharacter.java
@@ -0,0 +1,12 @@
+package com.company.start;
+
+import javax.swing.*;
+
+/**
+ * Created by dwarfawrrior on 6/10/2016.
+ */
+public class CreateNewCharacter {
+    public static void CreateNewCharacte(){
+        JOptionPane.showMessageDialog(null, "New character was created!");
+    }
+}
---
+++ src/main/java/com/company/start/headNote/HeadNote.java
@@ -0,0 +1,7 @@
+package com.company.start.headNote;
+
+/**
+ * Created by dwarfawrrior on 6/10/2016.
+ */
+public class HeadNote {
+}
